89a5a7271a5281e02bc7ac2c5f262b5050306202
Dariusz Depta <dariusz.depta@dmntk.io>
2021-12-10 11:49:05 +0100

    Refactored output messages.

--- java_big_decimal_128/src/sofware/engos/feel/arithmetic/Main.java
+++ java_big_decimal_128/src/sofware/engos/feel/arithmetic/Main.java
@@ -29,26 +29,25 @@ class LoanComparator {
   /**
    * Calculates equity36moPct = 1 - equity36Mo(loanAmt, rate, n, paymentAmt) / requestedAmt * 0.8
    */
-  BigDecimal equity36moPct(BigDecimal requestedAmt, BigDecimal loanAmt, BigDecimal rate, BigDecimal n, BigDecimal paymentAmt) {
-    System.out.print("\ncalculating equity36moPct:\n");
+  BigDecimal equity36moPct(BigDecimal requestedAmt, BigDecimal loanAmt, BigDecimal rate, BigDecimal equity36Mo, BigDecimal paymentAmt) {
+    System.out.print("\n>> calculating equity36moPct:\n");
+
     System.out.printf(">> requestedAmt = %s\n", requestedAmt);
     System.out.printf(">>      loanAmt = %s\n", loanAmt);
     System.out.printf(">>         rate = %s\n", rate);
-    System.out.printf(">>            n = %s\n", n);
-    System.out.printf(">>   paymentAmt = %s\n\n", paymentAmt);
+    System.out.printf(">>   equity36Mo = %s\n", equity36Mo);
+    System.out.printf(">>   paymentAmt = %s\n", paymentAmt);
 
-    BigDecimal equity36Mo = equity36Mo(loanAmt, rate, n, paymentAmt);
-    System.out.printf("equity36Mo(loanAmt, rate, n, paymentAmt) = %s\n", equity36Mo);
 
     BigDecimal equity36Mo_div_requestedAmt = equity36Mo.divide(requestedAmt, SCALE, ROUNDING_MODE);
-    System.out.printf("equity36Mo(loanAmt, rate, n, paymentAmt) / requestedAmt = %s\n", equity36Mo_div_requestedAmt);
+    System.out.printf("equity36Mo / requestedAmt = %s\n", equity36Mo_div_requestedAmt);
 
     BigDecimal equity36Mo_div_requestedAmt_mul_08 = equity36Mo_div_requestedAmt.multiply(dec_str("0.8"), MATH_CONTEXT);
-    System.out.printf("equity36Mo(loanAmt, rate, n, paymentAmt) / requestedAmt * 0.8 = %s\n", equity36Mo_div_requestedAmt_mul_08);
+    System.out.printf("equity36Mo / requestedAmt * 0.8 = %s\n", equity36Mo_div_requestedAmt_mul_08);
 
     BigDecimal result = DEC_1.subtract(equity36Mo_div_requestedAmt_mul_08, MATH_CONTEXT);
-    System.out.printf("1 - equity36Mo(loanAmt, rate, n, paymentAmt) / requestedAmt * 0.8 = %s\n", result);
-    System.out.printf("equity36moPct = %s\n", result);
+    System.out.printf("1 - equity36Mo / requestedAmt * 0.8 = %s\n", result);
+    System.out.printf("<< equity36moPct = %s\n", result);
 
     return result;
   }
@@ -57,11 +56,11 @@ class LoanComparator {
    * Calculates monthly payment = p * r/12/( 1 - (1 + r/12) ** -n)
    */
   BigDecimal monthlyPayment(BigDecimal p, BigDecimal r, BigDecimal n) {
-    System.out.print("\ncalculating monthlyPayment:\n");
+    System.out.print("\n>> calculating monthlyPayment:\n");
 
     System.out.printf(">> p = %s\n", p);
     System.out.printf(">> r = %s\n", r);
-    System.out.printf(">> n = %s\n\n", n);
+    System.out.printf(">> n = %s\n", n);
 
     BigDecimal r_div_12 = r.divide(DEC_12, SCALE, ROUNDING_MODE);
     System.out.printf("r/12 = %s\n", r_div_12);
@@ -83,7 +82,7 @@ class LoanComparator {
 
     BigDecimal result = p_mul_r_div_12.divide(one_minus_one_plus_r_div_12_pow_neg_n, SCALE, ROUNDING_MODE);
     System.out.printf("p * r / 12 / ( 1 - (1 + r/12) ** -n) = %s\n", result);
-    System.out.printf("monthlyPayment = %s\n", result);
+    System.out.printf("<< monthlyPayment = %s\n", result);
 
     return result;
   }
@@ -92,11 +91,11 @@ class LoanComparator {
    * Calculates loanAmt = requestedAmt * (1 + points/100) + fee
    */
   BigDecimal loanAmt(BigDecimal requestedAmt, BigDecimal points, BigDecimal fee) {
-    System.out.print("\ncalculating loanAmt:\n");
+    System.out.print("\n>> calculating loanAmt:\n");
 
     System.out.printf(">> requestedAmt = %s\n", requestedAmt);
     System.out.printf(">>       points = %s\n", points);
-    System.out.printf(">>          fee = %s\n\n", fee);
+    System.out.printf(">>          fee = %s\n", fee);
 
     BigDecimal points_div_100 = points.divide(DEC_100, SCALE, ROUNDING_MODE);
     System.out.printf("points/100 = %s\n", points_div_100);
@@ -109,7 +108,7 @@ class LoanComparator {
 
     BigDecimal result = requested_amt_mul_one_plus_points_div_100.add(fee, MATH_CONTEXT);
     System.out.printf("requestedAmt * (1 + points/100) + fee = %s\n", result);
-    System.out.printf("loanAmt = %s\n", result);
+    System.out.printf("<< loanAmt = %s\n", result);
 
     return result;
   }
@@ -118,12 +117,12 @@ class LoanComparator {
    * Calculates equity36Mo = p * (1 + r/12) ** n - pmt * (-1 + (1 + r/12) ** n) / r
    */
   BigDecimal equity36Mo(BigDecimal p, BigDecimal r, BigDecimal n, BigDecimal pmt) {
-    System.out.print("\ncalculating equity36Mo:\n");
+    System.out.print("\n>> calculating equity36Mo:\n");
 
     System.out.printf(">>   p = %s\n", p);
     System.out.printf(">>   r = %s\n", r);
     System.out.printf(">>   n = %s\n", n);
-    System.out.printf(">> pmt = %s\n\n", pmt);
+    System.out.printf(">> pmt = %s\n", pmt);
 
     BigDecimal r_div_12 = r.divide(DEC_12, SCALE, ROUNDING_MODE);
     System.out.printf("r/12 = %s\n", r_div_12);
@@ -147,8 +146,8 @@ class LoanComparator {
     System.out.printf("pmt * (-1 + (1 + r/12) ** n) / r = %s\n", pmt_mul_neg_one_one_plus_r_div_12_pow_n_div_r);
 
     BigDecimal result = p_mul_one_plus_r_div_12_pow_n.subtract(pmt_mul_neg_one_one_plus_r_div_12_pow_n_div_r, MATH_CONTEXT);
-    System.out.printf("p * (1 + r/12) ** n - pmt * (-1 + (1 + r/12) ** n) / r = %s\n\n", result);
-    System.out.printf("equity36Mo = %s\n\n", result);
+    System.out.printf("p * (1 + r/12) ** n - pmt * (-1 + (1 + r/12) ** n) / r = %s\n", result);
+    System.out.printf("<< equity36Mo = %s\n", result);
 
     return result;
   }
@@ -164,12 +163,11 @@ public class Main {
     BigDecimal rate = dec_str("0.03500");
     BigDecimal loanAmt = comparator.loanAmt(requestedAmt, points, fee);
     BigDecimal paymentAmt = comparator.monthlyPayment(loanAmt, rate, dec_int(360));
-    BigDecimal equity36moPct = comparator.equity36moPct(requestedAmt, loanAmt, rate, dec_int(36), paymentAmt);
-    System.out.printf(">> paymentAmt:test    = %s\n", "1481.847469769075");
-    System.out.printf(">> paymentAmt:D128    = %s\n", paymentAmt);
-    System.out.printf(">> paymentAmt:rust    = %s\n", "1481.8474697691209029114153224");
-    System.out.printf(">> equity36moPct:test = %s\n", "0.1229130806675829");
-    System.out.printf(">> equity36moPct:D128 = %s\n", equity36moPct);
-    System.out.printf(">> equity36moPct:rust = %s\n", "0.1229130806675864888391782027");
+    BigDecimal equity36Mo = comparator.equity36Mo(loanAmt, rate, dec_int(36), paymentAmt);
+    BigDecimal equity36moPct = comparator.equity36moPct(requestedAmt, loanAmt, rate, equity36Mo, paymentAmt);
+    System.out.println("\n\nRESULT");
+    System.out.println("--------------------------------------------------------------------------------------");
+    System.out.printf("  paymentAmt    = %s\n", paymentAmt);
+    System.out.printf("  equity36moPct = %s\n", equity36moPct);
   }
 }
